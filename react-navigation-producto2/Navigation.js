b7b7190072d21b93ca5168a637d5fdc1be775e81
Manuel Yerbes <mayerga4@gmail.com>
2022-04-18 19:31:37 +0200

    bottomTab creado

--- react-navigation-producto2/Navigation.js
+++ react-navigation-producto2/Navigation.js
@@ -3,10 +3,14 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 //Toda la navegación tiene que ir en un componente
 import {NavigationContainer} from "@react-navigation/native";
 
+
 //IMPORTANDO screens
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import StackScreen from "./screens/StackScreen";
+import { ColorPropType } from "react-native";
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 //Guardar en la variable la BottomTab
@@ -14,9 +18,32 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs(){
     return(
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Settings" component={SettingsScreen}/>
+        <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{tabBarActiveTintColor: 'purple'}}
+        >
+            <Tab.Screen 
+                name="Home" 
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Home', 
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="home" size={24} color="black" />
+                    ),
+                    tabBarBadge: 3,
+                    headerShown: false,
+                }}
+            />
+            <Tab.Screen 
+                name="Settings" 
+                component={SettingsScreen}
+                options={{
+                    tabBarLabel: 'Settings', 
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="brightness-5" size={24} color="black" />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
